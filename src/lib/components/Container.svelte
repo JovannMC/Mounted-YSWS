4bb5b60e1f566630b22f46d71e65ff48414135b8
Maya <jovannmc@femboyfurry.net>
2026-04-10 22:18:02 +0300

    feat: glassmorphism or something!!

---
+++ src/lib/components/Container.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { type Snippet } from 'svelte';
+
+	interface Props {
+		title?: string;
+		children: Snippet;
+	}
+
+	let { children }: Props = $props();
+</script>
+
+
+<div
+	class="h-[min(70vh,720px)] w-full max-w-[min(90vw,1280px)] overflow-hidden rounded-4xl border border-white/20 bg-white/10 backdrop-blur-[20px] backdrop-saturate-100"
+>
+	{@render children()}
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,22 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
+	import Container from '$lib/components/Container.svelte';
+	import bg from '$lib/assets/hc-hq-2.png';
 
 	onMount(() => {
 		console.log('meow!');
 	});
 </script>
+
+<img src={bg} alt="background" class="absolute inset-0 h-full w-full object-cover select-none" />
+
+<div class="relative z-10 flex min-h-screen w-full items-center justify-center p-10">
+	<Container>
+		<div class="h-full w-full p-6">
+			<h1 class="text-2xl font-bold text-white">meow meow!</h1>
+		</div>
+	</Container>
+</div>
 
 <style lang="scss">
 </style>
