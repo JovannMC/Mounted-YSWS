5119bf99d1f8a99b9e6032c28ff42be3b2bea703
Maya <jovannmc@femboyfurry.net>
2026-04-12 07:29:45 +0300

    feat: clearer 404

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,6 +1,16 @@
 <script lang="ts">
 	import { page } from '$app/state';
 	import BugOff from '@lucide/svelte/icons/bug-off';
+
+	const fake404 =
+		'Oops! Seems like your headset crashed with code 404. Try restarting it maybe?\r\n\r\n(not really - this page is actually not implemented for this demo)';
+	const realError = `Oops! Seems like your headset crashed with code ${page.status}. Try restarting it maybe?`;
+
+	const status = page.status;
+	const url = page.url.pathname;
+
+	// @ts-expect-error these are the fake pages in the sidebar lol
+	const isFake404 = status === 404 && (url === '/settings' || url === '/help');
 </script>
 
 <div class="flex h-full w-full flex-col items-center justify-center p-6 text-center">
@@ -8,8 +18,8 @@
 		<span><BugOff class="text-white/90" size={44} /></span>
 		<h1 class="text-4xl font-bold">ded headset</h1>
 	</div>
-	<p class="mx-auto mt-4 max-w-xl text-lg text-white/90">
-		Oops! Seems like your headset crashed with code {page.status}. Try restarting it maybe?
+	<p class="mx-auto mt-4 text-lg text-white/90 whitespace-pre-line">
+		{isFake404 ? fake404 : realError}
 	</p>
 
 	<!-- restart headset button (go back) -->
